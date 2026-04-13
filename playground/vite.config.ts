1fcf9b78fe3030ac90e7de7dc8e03e224f5a4846
Leah Joy Ishiguro <leah_ishiguro@jtekt.co.jp>
2026-04-13 11:36:58 +0900

    Playground improvement

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command }) => {
+  const isDev = command === "serve";
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        ...(isDev
+          ? {
+              group_manager_vue_picker: path.resolve(__dirname, "../src"),
+            }
+          : {}),
+      },
+    },
+  };
 });
